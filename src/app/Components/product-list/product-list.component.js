56331fd643e4ad3a35b9ab44442c550746a18f3a
jack <jackmonroy1000@gmail.com>
2021-05-28 10:45:57 -0700

    cambios en el product list y header

---
+++ src/app/Components/product-list/product-list.component.js
@@ -0,0 +1,9 @@
+var instance = M.FormSelect.getInstance(elem);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  instance.getSelectedValues();
+  instance.destroy();
