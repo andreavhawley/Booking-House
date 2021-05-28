2f8753b0909201233c536881f80836dec7aa5d51
jack <jackmonroy1000@gmail.com>
2021-05-28 16:02:15 -0700

    cambios jackie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './Components/home-page/home-page.component';
 import { ProductListComponent } from './Components/product-list/product-list.component';
+import { AboutComponent } from './Components/about/about.component';
 
 const routes: Routes = [
   { path:'', component: HomePageComponent},
-  { path:'productos', component: ProductListComponent}
+  { path:'productos', component: ProductListComponent},
+  { path:'nosotros', component: AboutComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomePageComponent } from './Components/home-page/home-page.component';
 import { ProductPageComponent } from './Components/product-page/product-page.component';
 import { ProductListComponent } from './Components/product-list/product-list.component';
 import { ProductComponent } from './Components/product/product.component';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { ProductComponent } from './Components/product/product.component';
     ProductPageComponent,
     ProductListComponent,
     ProductComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
