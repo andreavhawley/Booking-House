5a8a04841d9714f1123628e25363eddc27adee9d
Drarkusss0 <69326955+Drarkusss0@users.noreply.github.com>
2021-06-07 11:19:51 -0700

    Boton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './Components/home-page/home-page.component';
 import { ProductListComponent } from './Components/product-list/product-list.component';
 import { AboutComponent } from './Components/about/about.component';
 import { ContactComponent } from './Components/contact/contact.component';
 import { VendedorComponent } from './Components/vendedor/vendedor.component';
+import { ProductPageComponent } from './Components/product-page/product-page.component'
 
 const routes: Routes = [
   { path:'', component: HomePageComponent},
+  { path:'des-producto', component: ProductPageComponent},
   { path:'productos', component: ProductListComponent},
   { path:'nosotros', component: AboutComponent},
   { path:'contacto', component: ContactComponent},
